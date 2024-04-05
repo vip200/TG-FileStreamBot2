2ab8793e18a9e16432ef348b623918c3d75848da
vip200 <51438486+vip200@users.noreply.github.com>
2024-04-05 19:16:27 +0300

    Add files via upload

--- WebStreamer/vars.py
+++ WebStreamer/vars.py
@@ -8,12 +8,12 @@ load_dotenv()
 
 class Var(object):
     MULTI_CLIENT = False
-    API_ID = int(environ.get("API_ID"))
-    API_HASH = str(environ.get("API_HASH"))
-    BOT_TOKEN = str(environ.get("BOT_TOKEN"))
+    API_ID = 22191690#int(environ.get("API_ID"))
+    API_HASH = '52f7f2fd2a96132679eae22129f6f720'#str(environ.get("API_HASH"))
+    BOT_TOKEN = '5010315993:AAG5RNiWdN4LepyTgP61DoReM89HoLyUCtg'#str(environ.get("BOT_TOKEN"))
     SLEEP_THRESHOLD = int(environ.get("SLEEP_THRESHOLD", "60"))  # 1 minte
     WORKERS = int(environ.get("WORKERS", "6"))  # 6 workers = 6 commands at once
-    BIN_CHANNEL = int(
+    BIN_CHANNEL = -1001689905120#int(
         environ.get("BIN_CHANNEL", None)
     )  # you NEED to use a CHANNEL when you're using MULTI_CLIENT
     PORT = int(environ.get("PORT", 8080))
@@ -23,11 +23,11 @@ class Var(object):
     HAS_SSL = True if str(HAS_SSL).lower() == "true" else False
     NO_PORT = environ.get("NO_PORT", False)
     NO_PORT = True if str(NO_PORT).lower() == "true" else False
-    if "DYNO" in environ:
-        ON_HEROKU = True
-        APP_NAME = str(environ.get("APP_NAME"))
-    else:
-        ON_HEROKU = False
+    # if "DYNO" in environ:
+    ON_HEROKU = True
+    APP_NAME = 'rrr333'#str(environ.get("APP_NAME"))
+    # else:
+        # ON_HEROKU = False
     FQDN = (
         str(environ.get("FQDN", BIND_ADDRESS))
         if not ON_HEROKU or environ.get("FQDN")
@@ -40,7 +40,7 @@ class Var(object):
             "s" if HAS_SSL else "", FQDN, "" if NO_PORT else ":" + str(PORT)
         )
 
-    UPDATES_CHANNEL = str(environ.get('UPDATES_CHANNEL', "aredirect"))
-    OWNER_ID = int(environ.get('OWNER_ID', '777000'))
+    UPDATES_CHANNEL = '-1001689905120'#str(environ.get('UPDATES_CHANNEL', "aredirect"))
+    OWNER_ID = 384403734#int(environ.get('OWNER_ID', '777000'))
 
-    BANNED_CHANNELS = list(set(int(x) for x in str(environ.get("BANNED_CHANNELS", "-1001296894100")).split()))
+    BANNED_CHANNELS = -1001689905120#list(set(int(x) for x in str(environ.get("BANNED_CHANNELS", "-1001296894100")).split()))
