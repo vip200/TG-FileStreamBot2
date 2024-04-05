0d034d68d8daf5a2b653772301d1c63e9edd1e54
vip200 <51438486+vip200@users.noreply.github.com>
2024-04-06 00:02:23 +0300

    Add files via upload

--- WebStreamer/server/stream_routes.py
+++ WebStreamer/server/stream_routes.py
@@ -145,7 +145,7 @@ async def media_streamer(request: web.Request, message_id: int, secure_hash: str
         body=body,
         headers={
             "Content-Type": f"{mime_type}",
-            "Range": f"bytes={from_bytes}-{until_bytes}",
+            # "Range": f"bytes={from_bytes}-{until_bytes}",
             "Content-Range": f"bytes {from_bytes}-{until_bytes}/{file_size}",
             "Content-Length": str(file_size),
             "Content-Disposition": f'{disposition}; filename="{file_name}"',
