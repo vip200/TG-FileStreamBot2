ec7034b96890305c848025d315d7aee28c4c9b39
vip200 <51438486+vip200@users.noreply.github.com>
2024-04-06 01:09:51 +0300

    Add files via upload

--- WebStreamer/server/stream_routes.py
+++ WebStreamer/server/stream_routes.py
@@ -140,7 +140,7 @@ async def media_streamer(request: web.Request, message_id: int, secure_hash: str
         else:
             mime_type = "application/octet-stream"
             file_name = f"{secrets.token_hex(2)}.unknown"
-            
+    print('433333333333333333333333333: '+str(file_size))
     return_resp = web.Response(
         status=206 if range_header else 200,
         body=body,
@@ -148,7 +148,7 @@ async def media_streamer(request: web.Request, message_id: int, secure_hash: str
             "Content-Type": f"{mime_type}",
             "Range": f"bytes={from_bytes}-{until_bytes}",# ביטלתי
             "Content-Range": f"bytes {from_bytes}-{until_bytes}/{file_size}",
-            # "Content-Length": str(file_size),# והוספתי את זה 
+            "Content-Length": str(file_size),# והוספתי את זה 
             "Content-Disposition": f'{disposition}; filename="{file_name}"',
             "Accept-Ranges": "bytes",
         },
