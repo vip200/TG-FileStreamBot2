0521d1ba83fa9b9558d4267fee7232bd4b50188f
vip200 <51438486+vip200@users.noreply.github.com>
2024-04-06 23:02:24 +0300

    Add files via upload

--- WebStreamer/bot/plugins/callback.py
+++ WebStreamer/bot/plugins/callback.py
@@ -48,7 +48,7 @@ async def cb_data(bot, update: CallbackQuery):
         elif usr_cmd[0] == "msgdelno":
             get_msg = await bot.get_messages(chat_id=Var.BIN_CHANNEL, message_ids=int(usr_cmd[1]))
             if get_media_file_unique_id(get_msg) == usr_cmd[2]:
-                await asyncio.sleep(1)# ANONYMOUS
+                await asyncio.sleep(10)# ANONYMOUS
                 reply_markup, Stream_Text, stream_link = await gen_link(m=update, log_msg=get_msg, from_channel=False)
 
                 await update.message.edit_text(
