5fe7c432b2b60cbe9db04c83fbf04317a805c0c3
vip200 <51438486+vip200@users.noreply.github.com>
2024-04-06 20:41:24 +0300

    Add files via upload

--- WebStreamer/utils/file_properties.py
+++ WebStreamer/utils/file_properties.py
@@ -82,12 +82,12 @@ async def gen_link(m: Message,log_msg: Messages, from_channel: bool):
     page_link = f"{Var.URL}watch/{get_hash(log_msg)}{log_msg.id}"
     stream_link = f"{Var.URL}{log_msg.id}/{quote_plus(get_name(m))}?hash={get_hash(log_msg)}"
     Stream_Text=lang.stream_msg_text.format(file_name, file_size, stream_link, page_link)
-    reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("🖥STREAM", url=page_link), InlineKeyboardButton("Dᴏᴡɴʟᴏᴀᴅ 📥", url=stream_link)]])
+    reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("🖥 צפייה ישירה", url=page_link), InlineKeyboardButton("📥 הורד את הקובץ", url=stream_link)]])
 
     if from_channel:
-        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("🖥STREAM", url=page_link), InlineKeyboardButton("Dᴏᴡɴʟᴏᴀᴅ 📥", url=stream_link)]])
+        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("🖥 צפייה ישירה", url=page_link), InlineKeyboardButton("📥 הורד את הקובץ", url=stream_link)]])
     else:
-        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("🖥STREAM", url=page_link), InlineKeyboardButton("Dᴏᴡɴʟᴏᴀᴅ 📥", url=stream_link)],
+        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("🖥 צפייה ישירה", url=page_link), InlineKeyboardButton("📥 הורד את הקובץ", url=stream_link)],
             [InlineKeyboardButton("❌ Delete Link", callback_data=f"msgdelconf2_{log_msg.id}_{get_media_file_unique_id(log_msg)}")]])
 
     return reply_markup, Stream_Text, stream_link
